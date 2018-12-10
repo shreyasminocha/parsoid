10859ec53ccfad46b3c73eb1b8d5b97003ea9ee3
Subramanya Sastry <ssastry@wikimedia.org>
2018-12-10 17:26:59 -0600

    Port addExtLinkClasses to PHP
    * UNTESTED quick port from js -> php to test
      git log --follow behavior.
    
    Change-Id: Ib73afe05f271cc254f58ae3de39c5072e1de2e09

--- php/lib/wt2html/pp/processors/addExtLinkClasses.php
+++ php/lib/wt2html/pp/processors/addExtLinkClasses.php
@@ -1,36 +1,37 @@
-'use strict';
+<?php
+namespace Parsoid\Lib\Wt2Html\PP\Processors;
 
-var DU = require('../../../utils/DOMUtils.js').DOMUtils;
+require_once __DIR__."/../../../utils/DU.php";
+
+use Parsoid\Lib\Utils\DU;
 
 /**
  * Adds a new attribute name and value immediately after an
  * attribute specified in afterName. If afterName is not found
  * the new attribute is appended to the end of the list.
  */
-function insertAfter(node, afterName, newName, newVal) {
-	// ensure existing attribute of newName doesn't interfere
+function insertAfter($node, $afterName, $newName, $newVal) {
+	// ensure existing attribute of $newName doesn't interfere
 	// with desired positioning
-	node.removeAttribute(newName);
-	// make a JS array from the DOM NamedNodeList
-	var attributes = Array.from(node.attributes);
-	// attempt to find the afterName
-	var where = 0;
-	for (; where < attributes.length; where++) {
-		if (attributes[where].name === afterName) {
+	$node->removeAttribute($newName);
+	$attributes = $node->attributes;
+	// attempt to find the $afterName
+	$where = 0;
+	for (; $where < count($attributes); $where++) {
+		if ($attributes[$where]->name === $afterName) {
 			break;
 		}
 	}
-	// if we found the afterName key, then removing them from the DOM
-	var i;
-	for (i = where + 1; i < attributes.length; i++) {
-		node.removeAttribute(attributes[i].name);
+	// if we found the $afterName key, then removing them from the DOM
+	for ($i = $where + 1; $i < count($attributes); $i++) {
+		$node->removeAttribute($attributes[$i]->name);
 	}
 	// add the new attribute
-	node.setAttribute(newName, newVal);
+	$node->setAttribute($newName, $newVal);
 
 	// add back all stored attributes that were temporarily removed
-	for (i = where + 1; i < attributes.length; i++) {
-		node.setAttribute(attributes[i].name, attributes[i].value);
+	for ($i = $where + 1; $i < count($attributes); $i++) {
+		$node->setAttribute($attributes[$i]->name, $attributes[$i]->value);
 	}
 }
 
@@ -39,29 +40,25 @@ function insertAfter(node, afterName, newName, newVal) {
  * Currently positions the class immediately after the rel attribute
  * to keep tests stable.
  */
-function addExtLinkClasses(env, document) {
-	var extLinks = document.body.querySelectorAll('a[rel~="mw:ExtLink"]');
-	extLinks.forEach(function(a) {
-		var classInfoText = 'external autonumber';
-		if (a.firstChild) {
-			classInfoText = 'external text';
+function addExtLinkClasses($env, $document) {
+	$extLinks = $document->body->querySelectorAll('a[rel~="mw:ExtLink"]');
+	foreach ($extLinks as $a) {
+		$classInfoText = 'external autonumber';
+		if ($a->firstChild) {
+			$classInfoText = 'external text';
 			// The "external free" class is reserved for links which
 			// are syntactically unbracketed; see commit
 			// 65fcb7a94528ea56d461b3c7b9cb4d4fe4e99211 in core.
-			if (DU.usesURLLinkSyntax(a)) {
-				classInfoText = 'external free';
-			} else if (DU.usesMagicLinkSyntax(a)) {
+			if (DU::usesURLLinkSyntax($a)) {
+				$classInfoText = 'external free';
+			} else if (DU::usesMagicLinkSyntax($a)) {
 				// PHP uses specific suffixes for RFC/PMID/ISBN (the last of
 				// which is an internal link, not an mw:ExtLink), but we'll
 				// keep it simple since magic links are deprecated.
-				classInfoText = 'external mw-magiclink';
+				$classInfoText = 'external mw-magiclink';
 			}
 		}
 
-		insertAfter(a, 'rel', 'class', classInfoText);
+		insertAfter($a, 'rel', 'class', $classInfoText);
 	});
-}
-
-if (typeof module === 'object') {
-	module.exports.addExtLinkClasses = addExtLinkClasses;
 }
